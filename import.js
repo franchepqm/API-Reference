b4e2e7b1cfd681c0687514c2e0ef8badf2bf0783
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-06-17 02:23:20 -0300

    Update import.js

--- import.js
+++ import.js
@@ -1,1 +1,9 @@
 'https://www.gstatic.com/firebasejs/10.4.0/firebase-SERVICE.js'
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-storage.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-functions.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-analytics.js";
